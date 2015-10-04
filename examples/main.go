b49b892303384fdfbc0a7e6f5dd9894ee6489f68
Guy Fergusson <fergs@iinet.net.au>
2015-10-04 13:15:51 +0800

    updated example

--- examples/main.go
+++ examples/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/fergstar/go-parse/parse"
 )
 
+type config struct {
+	ParseApplicationID string `envconfig:"APPLICATION_ID"`
+	ParseRestAPIKey    string `envconfig:"RESTAPI_KEY"`
+}
+
 func main() {
 
 	http_client := &http.Client{}
