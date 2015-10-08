6d7ea04e6d4dd965b6c3e4eadb8a7ab503b5af2c
Guy Fergusson <fergs@iinet.net.au>
2015-10-09 00:51:20 +0800

    updated errors to show api errors

--- parse/errors.go
+++ parse/errors.go
@@ -6,17 +6,30 @@ import (
 
 // APIError represents a Parse API Error response.
 type APIError struct {
-	Code  string `json:"code"`
-	Error string `json:"error"`
+	Code         string `json:"code"`
+	ErrorMessage string `json:"error"`
 }
 
 // Message displays the Code and Error as string message.
-func (e *APIError) Message() string {
+func (e *APIError) Error() string {
 
-	if len(e.Error) > 0 {
+	if len(e.ErrorMessage) > 0 {
 		return fmt.Sprintf("parse: %d %v", e.Code, e.Error)
 	}
 
 	return ""
 
+}
+
+func releventError(httpError error, apiError *APIError) error {
+
+	if httpError != nil {
+		return httpError
+	}
+
+	if apiError.ErrorMessage == "" {
+		return nil
+	}
+
+	return apiError
 }
