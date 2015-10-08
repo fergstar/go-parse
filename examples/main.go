630c9cfa9a962bd836a19c5351600bdaaeb72a34
Guy Fergusson <fergs@iinet.net.au>
2015-10-09 00:44:25 +0800

    updated example

--- examples/main.go
+++ examples/main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/fergstar/go-parse/parse"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+	"net/http"
 )
 
 type config struct {
-	ParseApplicationID string `envconfig:"APPLICATION_ID"`
-	ParseRestAPIKey    string `envconfig:"RESTAPI_KEY"`
+	ParseApplicationID string `envconfig:"PARSE_APPLICATION_ID"`
+	ParseRestAPIKey    string `envconfig:"PARSE_REST_API_KEY"`
+}
+
+type GameScore struct {
+	Name string
 }
 
 func main() {
+	var c config
+	err := envconfig.Process("PARSE", &c)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if c.ParseApplicationID == "" || c.ParseRestAPIKey == "" {
+		log.Fatal("Missing required environment variable")
+	}
 
 	http_client := &http.Client{}
 
 	// Parse
 
-	client := parse.NewClient(http_client)
+	objectBody := &GameScore{Name: "Test"}
 
+	client := parse.NewClient(http_client, c.ParseApplicationID, c.ParseRestAPIKey)
+
+	success, _, err := client.Objects.Create("gamescore", objectBody)
+
+	if err != nil {
+		log.Fatal("Error: %v", err)
+	}
+
+	fmt.Println(success.ObjectID)
 }
