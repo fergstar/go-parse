93e52e4172d45ebc181ae7df77f9bfc2d4fc1b85
Guy Fergusson <fergs@iinet.net.au>
2015-10-10 16:25:30 +0800

    added update and delete

--- parse/objects.go
+++ parse/objects.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/dghubble/sling"
 )
@@ -22,16 +23,21 @@ func NewObjectService(sling *sling.Sling) *ObjectService {
 	}
 }
 
-// Success represents a Parse API Object Succes response
-type Success struct {
+// SuccessCreate represents a Parse API Object Succes response
+type SuccessCreate struct {
 	CreatedAt string `json:"createdAt"`
 	ObjectID  string `json:"objectId"`
 }
 
-// Create a new object on the Parse Cloud.
-func (o *ObjectService) Create(className string, objectBody interface{}) (*Success, *http.Response, error) {
+// SuccessUpdate represents a Parse API Object Update successful update response
+type SuccessUpdate struct {
+	UpdatedAt time.Time `json:"updatedAt"`
+}
 
-	success := new(Success)
+// Create a new object on the Parse Cloud.
+func (o *ObjectService) Create(className string, objectBody interface{}) (*SuccessCreate, *http.Response, error) {
+
+	success := new(SuccessCreate)
 	apiError := new(APIError)
 	path := fmt.Sprintf("%s", className)
 	resp, err := o.sling.New().Post(path).BodyJSON(objectBody).Receive(success, apiError)
@@ -39,7 +45,7 @@ func (o *ObjectService) Create(className string, objectBody interface{}) (*Succe
 }
 
 // Retrieve an object from the Parse Cloud.
-func (o *ObjectService) Retrieve(className string, objectID string) (*json.RawMessage, *http.Response, error) {
+func (o *ObjectService) Retrieve(className, objectID string) (*json.RawMessage, *http.Response, error) {
 
 	var parseObject *json.RawMessage = &json.RawMessage{}
 	apiError := new(APIError)
@@ -49,14 +55,22 @@ func (o *ObjectService) Retrieve(className string, objectID string) (*json.RawMe
 	return parseObject, resp, releventError(err, apiError)
 }
 
-/*
 // Update the data on an object that already exists.
-func (o *ObjectService) Update(className string) {
+func (o *ObjectService) Update(className, objectID string, objectBody interface{}) (*SuccessUpdate, *http.Response, error) {
+	success := new(SuccessUpdate)
+	apiError := new(APIError)
+	path := fmt.Sprintf("%s/%s", className, objectID)
+	resp, err := o.sling.New().Put(path).BodyJSON(objectBody).Receive(success, apiError)
 
+	return success, resp, releventError(err, apiError)
 }
 
 // Delete an object from the Parse Cloud.
-func (o *ObjectService) Delete(className string) {
+func (o *ObjectService) Delete(className, objectID string) (*http.Response, error) {
 
+	apiError := new(APIError)
+	path := fmt.Sprintf("%s/%s", className, objectID)
+	resp, err := o.sling.New().Delete(path).Receive(nil, apiError)
+
+	return resp, releventError(err, apiError)
 }
-*/
